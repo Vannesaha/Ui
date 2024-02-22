3f72e5a443cc5c100f14537a127386ad6125273c
asku1990 <a-p.kvs@pm.me>
2024-02-22 15:54:08 +0200

    delete publish_message becouse it wasn't needed

--- config/settings.py
+++ config/settings.py
@@ -7,7 +7,7 @@ DEVICE_2 = "pneumatic"  # for other devices
 DEVICE_3 = "electrical"  # for other devices
 
 STATUS_TOPICS = {
-    f"device/{DEVICE_1}": f"Are you ready {DEVICE_1}?",
+    f"status/{DEVICE_1}": f"Are you ready {DEVICE_1}?",
 }
 HYDRAULIC_RESPONSE_TOPIC = (
     f"response/device/{DEVICE_1}"  # response topic for this device
